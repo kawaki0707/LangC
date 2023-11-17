47f6d930e2daa49a7727ebc5dedfc941e48b48ec
Masaki Kawano <d2531016@eng.kitakyu-u.ac.jp>
2023-11-17 14:32:15 +0900

    Cpro-2

--- main.c
+++ main.c
@@ -3,5 +3,16 @@
 int main(){
     printf("Hello World!\n");
     printf("Hello Masaki\n");
+
+    printf("I'm from\n");
+    printf("Fukuoka prefecture\n");
+    printf("I'm from\n");
+    printf("Tokyo prefecture\n");
+
+    int i,a=0;
+    for(i=0; i<10; i++){
+        a+=1;
+        printf("%d\n",a);
+    }
     return 0;
     }
---
+++ main2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int price;
+    price=10;
+    printf("%d円見つけた\n",price);
+    return 0;
+}
