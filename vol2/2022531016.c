e60eab89e0ee1cd6cf141ce6e566ab29aec34ba5
Masaki Kawano <d2531016@eng.kitakyu-u.ac.jp>
2023-11-24 14:14:25 +0900

    第２回演習課題

---
+++ vol2/2022531016.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    printf("Welcome to C Programming!\n");
+
+    int x,y,z;
+    x=10;
+    y=2*x;
+    z=x+y;
+    printf("x=%d\ny=%d\nz=%d\n",x,y,z);
+
+    int price;
+    price=500;
+    printf("%f\n",price*1.1);
+    
+}
